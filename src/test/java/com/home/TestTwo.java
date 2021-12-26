974c3fe9181949efa979b5cda3bf6429bf5dc3d4
catcat4er <kerimov-t90@mail.ru>
2021-12-26 15:35:14 +0400

    Added validations

--- src/test/java/com/home/TestTwo.java
+++ src/test/java/com/home/TestTwo.java
@@ -1,28 +1,29 @@
 package com.home;
 
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
-import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.CollectionCondition.containExactTextsCaseSensitive;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestTwo {
 
     @BeforeAll
-    static void Size () {
+    static void Size() {
         Configuration.browserSize = "1920x1040";
     }
-
 
     @Test
     void textBox() {
         open("https://demoqa.com/automation-practice-form");
-
 
         $("#firstName").setValue("Name");
         $("#lastName").setValue("Surname");
@@ -35,11 +36,30 @@ public class TestTwo {
         $(".react-datepicker__day--022").click();
         $("#subjectsInput").setValue("Computer Science").pressEnter();
         $("#hobbiesWrapper").$(byText("Reading")).click();
-        $("#uploadPicture").uploadFile(new File("src/test/java/com/home/Penguins.jpg"));
+        $("#uploadPicture").uploadFile(new File("src/test/resources/Penguins.jpg"));
         $("#currentAddress").setValue("address");
         $("#react-select-3-input").setValue("NCR").pressEnter();
         $("#react-select-4-input").setValue("Delhi").pressEnter();
         $("#submit").scrollTo().click();
-        $("#closeLargeModal").scrollTo().click();
+
+        $$(".modal-content td").shouldHave(containExactTextsCaseSensitive("Name Surname", "mail@mail.ru",
+                "Male", "8987654321", "22 June,1941", "Computer Science", "Reading", "Penguins.jpg",
+                "address", "NCR Delhi"));
+
+//        $(".table-responsive").shouldHave(        //much longer type
+//                text("Name"),
+//                text("Surname"),
+//                text("mail@mail.ru"),
+//                text("Male"),
+//                text("8987654321"),
+//                text("22 June,1941"),
+//                text("Computer Science"),
+//                text("Reading"),
+//                text("Penguins.jpg"),
+//                text("address"),
+//                text("NCR Delhi"));
+
+        Selenide.closeWebDriver();
+
     }
 }
